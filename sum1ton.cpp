1d9addf4b00acc0989c3394e61dc7b38e9c83471
aman6946 <amansingh6946@gmail.com>
2025-12-24 10:17:33 +0530

    recursion

---
+++ sum1ton.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+    if(n==0){
+        return n;
+
+    }
+
+    return n+sum(n-1);
+}
+int main(){
+    
+   int a= sum(5);
+   cout<<a;
+    
+
+}
